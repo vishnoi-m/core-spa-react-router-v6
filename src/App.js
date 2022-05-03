d00a40dcc28707c3e25343cbb46f2e603f8e13a2
Mayank Vishnoi <mayankvishnoi@Mayanks-MacBook-Air.local>
2022-05-03 23:20:17 +0530

    core-react-router-version-6

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/" exact element={<Navigate to="/welcome" />} />
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route path="new-user" element={<p>Welcome, New User!!</p>} />
+          </Route>
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page.</h1>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Welcome;
